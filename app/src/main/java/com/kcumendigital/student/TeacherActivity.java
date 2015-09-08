c2560162cf1442d843d16fc9c139d7aac954d57f
unknown <manuel.ramirez1209@gmail.com>
2015-09-08 17:54:16 -0500

    cambios fragments

--- app/src/main/java/com/kcumendigital/student/TeacherActivity.java
+++ app/src/main/java/com/kcumendigital/student/TeacherActivity.java
@@ -1,37 +1,25 @@
 package com.kcumendigital.student;
 
+
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.kcumendigital.student.fragments.GestionDocente;
 
 public class TeacherActivity extends AppCompatActivity {
+
+    GestionDocente docente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_teacher, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        docente = new GestionDocente();
+        FragmentTransaction ftr = getSupportFragmentManager().beginTransaction();
+        ftr.replace(R.id.container_docente,docente);
+        ftr.commit();
+        setContentView(R.layout.activity_teacher);
     }
 }
--- app/src/main/java/com/kcumendigital/student/fragments/GestionDocente.java
+++ app/src/main/java/com/kcumendigital/student/fragments/GestionDocente.java
@@ -2,32 +2,27 @@ package com.kcumendigital.student.fragments;
 
 
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.kcumendigital.student.R;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GestionEstudiantes extends Fragment {
+public class GestionDocente extends Fragment {
 
-    Button btn_cursos, btn_materias, btn_horarios, btn_examenes;
 
-    public GestionEstudiantes() {}
+    public GestionDocente() {}
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_gestion_estudiantes, container, false);
+        return inflater.inflate(R.layout.fragment_gestion_docente, container, false);
     }
 
 
