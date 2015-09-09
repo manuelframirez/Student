293fc80384dc477fe03ec006c93d4e1910bb0e36
manuelframirez <manuel.ramirez.v@gmail.com>
2015-09-09 13:52:20 -0500

    integracion fragment notas

--- app/src/main/java/com/kcumendigital/student/NoteActivity.java
+++ app/src/main/java/com/kcumendigital/student/NoteActivity.java
@@ -1,13 +1,17 @@
 package com.kcumendigital.student;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
+import com.kcumendigital.student.fragments.CursosFragment;
 
 public class NoteActivity extends AppCompatActivity {
+
+    CursosFragment cursosFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,13 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note);
 
         ((AnalyticsAplication) getApplication()).getTracker(AnalyticsAplication.TrackerName.APP_TRACKER);
+
+        cursosFragment = new CursosFragment();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container_notas, cursosFragment);
+        ft.commit();
+
     }
 
     @Override
